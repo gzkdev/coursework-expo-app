ec9fbeddb936b09e8aadb38a72a83128c542b41e
gzkdev <ezihegodswill01@gmail.com>
2023-05-17 17:14:04 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <SafeAreaView style={styles.container}>
+    //   {/* <Text>Hello React Native, Let's go!!!</Text>
+    //   <TouchableWithoutFeedback onPress={() => console.log("pressed")}>
+    //     <Image
+    //       source={{
+    //         width: 200,
+    //         height: 200,
+    //         uri: "https://picsum.photos/200",
+    //       }}
+    //       resizeMode="contain"
+    //     />
+    //   </TouchableWithoutFeedback>
+    //   <Button title="Click me" /> */}
+    //   <View style={styles.topBox}>
+    //     <Image style={styles.logo} source={require(".app/assets/icon.png")} />
+    //     <Text>Sell what you don't need</Text>
+    //   </View>
+    //   <View></View>
+    //   <View style={styles.bottomBox}>
+    //     <TouchableWithoutFeedback>
+    //       <View
+    //         style={{
+    //           backgroundColor: "yellow",
+    //           height: 40,
+    //           width: "100%",
+    //           alignItems: "center",
+    //           justifyContent: "center",
+    //         }}
+    //       >
+    //         <Text>Hello</Text>
+    //       </View>
+    //     </TouchableWithoutFeedback>
+    //     <TouchableWithoutFeedback>
+    //       <View
+    //         style={{
+    //           backgroundColor: "green",
+    //           height: 40,
+    //           width: "100%",
+    //           alignItems: "center",
+    //           justifyContent: "center",
+    //         }}
+    //       >
+    //         <Text>Hello</Text>
+    //       </View>
+    //     </TouchableWithoutFeedback>
+    //   </View>
+    //   <StatusBar style="auto" />
+    // </SafeAreaView>
+    // <SafeAreaView style={{ flex: 1 }}>
+
+    // </SafeAreaView>
+    // <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     display: "flex",
+//     backgroundColor: "#fff",
+//   },
+//   topBox: {
+//     position: "absolute",
+//     width: "100%",
+//     top: 80,
+//     left: 0,
+//     alignItems: "center",
+//     gap: 8,
+//   },
+//   bottomBox: {
+//     width: "100%",
+//     position: "absolute",
+//     bottom: 40,
+//     left: 0,
+//   },
+//   logo: {
+//     width: 100,
+//     height: 100,
+//     resizeMode: "contain",
+//   },
+// });
---
+++ app/screens/ViewImageScreen.jsx
@@ -0,0 +1,46 @@
+import { Image, StyleSheet, View } from "react-native";
+
+import colors from "../config/colors";
+
+const ViewImageScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    position: "absolute",
+    top: 40,
+    left: 30,
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+  deleteIcon: {
+    position: "absolute",
+    top: 40,
+    right: 30,
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ViewImageScreen;
